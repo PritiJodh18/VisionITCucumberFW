b84483021846015cce0f1468f70103d28d73752f
PritiJodh18 <pritisjodh@gmail.com>
2020-09-24 11:22:48 +0530

    "Adding Java Interview Program"

---
+++ src/test/java/runner/String.java
@@ -0,0 +1,17 @@
+package runner;
+
+public class String {
+	 public String(java.lang.String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String args[]){  
+		   java.lang.String s1="Sachin";  
+		   java.lang.String s2="Sachin";  
+		   String s3=new String("Sachin");  
+		   java.lang.String s4="Saurav";  
+		   System.out.println(s1.equals(s2));//true  
+		   System.out.println(s1.equals(s3));//true  
+		   System.out.println(s1.equals(s4));//false  
+		 }  
+}
